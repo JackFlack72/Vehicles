db33f8d836692e2c6a9f9c8782a6fa0447600f36
lucas.rappatta24@outlook.com <lucas.rappatta24@outlook.com>
2025-10-28 10:47:06 -0500

    add parent class field members and getters/setters

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class Vehicle {
+    private String color;
+    private int numberOfPassengers;
+    private int cargoCapacity;
+    private int fuelCapacity;
+
+    public static void main(String[] args) {
+        Moped slowRide = new Moped();
+        slowRide.setColor("Red");
+        slowRide.setFuelCapacity(5);
+
+        Car fastRide = new Car();
+        fastRide.setColor("Blue");
+        fastRide.setFuelCapacity(35);
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumberOfPassengers() {
+        return numberOfPassengers;
+    }
+
+    public void setNumberOfPassengers(int numberOfPassengers) {
+        this.numberOfPassengers = numberOfPassengers;
+    }
+
+    public int getCargoCapacity() {
+        return cargoCapacity;
+    }
+
+    public void setCargoCapacity(int cargoCapacity) {
+        this.cargoCapacity = cargoCapacity;
+    }
+
+    public int getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(int fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+}
